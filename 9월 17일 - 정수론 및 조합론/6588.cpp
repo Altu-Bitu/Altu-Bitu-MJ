cc1ff7e1ec217e9b372f2553acaa7e722c21a423
spiritstone <mjk4534@gmail.com>
2021-09-23 22:38:35 +0900

    [정수론 및 조합론] 9월 17일 -ing

---
+++ 9월 17일 - 정수론 및 조합론/6588.cpp
@@ -0,0 +1,32 @@
+/*
+ * [백준] 6588번 골드바흐의 추측
+ * 1. n이 큰 수이기 때문에 n까지의 소수를 빠르게 판단할 수 있는 '에라토스테네스의 체' 사용
+ * 2.
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+const int MAX = 1000001;
+vector <int> prime; // 소수 저장할 벡터
+int minPrime[MAX];
+
+// 소수 판단을 위한 에라토스테네스의 채
+void eratosthenes(void)
+{
+
+}
+
+
+int main(void) {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    int n; // 각 테스트케이스에 입력될 짝수 정수 n
+
+
+
+
+    return 0;
+}

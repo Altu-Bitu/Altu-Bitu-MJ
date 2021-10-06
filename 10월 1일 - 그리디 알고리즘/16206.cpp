69655a9e7ed8a86bf71964cc4223b60c3384873c
spiritstone <mjk4534@gmail.com>
2021-10-06 20:19:09 +0900

    [그리디 알고리즘] 10월 1일 -ing
    16206번 제출합니다.

---
+++ 10월 1일 - 그리디 알고리즘/16206.cpp
@@ -0,0 +1,66 @@
+//
+// Created by user on 2021-10-06.
+//
+/*
+ * [백준] 16206 - 롤케이크
+ * 입력 - N: 롤케이크 개수, M: 자를 수 있는 최대 횟수
+ *  - N개만큼의 롤케이크의 길이
+ *  출력: 만들 수 있는 길이가 10인 롤케이크 개수의 최대값
+ */
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n, m;
+int len, ans = 0; // len: 롤케이크의 길이, ans: 롤케이크 개수 저장값
+vector <int> cakeLen, cnt; // cakeLen: N개만큼의 롤케이크 길이 저장, cnt: 마지막 연산
+
+
+// 케이크 자르는 함수
+void cut(int len) {
+    if (m <= 0) // m이 0이하이면 더이상 케이크를 자를 수 없기 때문에 종료
+        return;
+    else {
+        len -= 10; // 길이를 10만큼 줄이면 (케이크를 자르면)
+        ans++; // 길이가 10인 롤케이크의 개수는 한 개 늘어나고
+        m--; // 자를 수 있는 횟수는 줄어든다.
+
+        if (len > 10) // 케이크 길이가 10 이상이면 또 자를 수 있음
+            cut(len); // 재귀함수
+        else if (len == 10) //케이크의 길이가 10이라면 자를 필요가 없으므로 그냥
+            ans++; // 정답 개수만 한 개 증가
+        return; // 종료
+    }
+}
+
+int main(void) {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> len;
+        if (len < 10) // 길이가 10 미만이면 계산할 필요가 없다.
+            continue;
+        cakeLen.push_back(len); // 입력받은 값 저장
+    }
+
+    sort(cakeLen.begin(), cakeLen.end()); // 길이 정렬
+
+    for (int i = 0; i < cakeLen.size(); i++) {
+        // cout << cakeLen[i] << " ";
+        if (m <= 0) // m이 0이하이면 더이상 케이크를 자를 수 없기 때문에 종료
+            break;
+        else if (cakeLen[i] == 10) //케이크의 길이가 10이라면 자를 필요가 없으므로 그냥
+            ans++;  // 정답 개수만 한 개 증가
+        else if (cakeLen[i] % 10 == 0) // 10의 배수이면 바로 함수 돌리기
+            cut(cakeLen[i]);
+        else cnt.push_back(cakeLen[i]); // m이 1 이상이고, 길이가 10 이상인데 10의 배수가 아닌 경우
+        // cut(cakeLen[i]);
+    }
+
+    for (int i = 0; i < cnt.size(); i++)
+        cut(cnt[i]);
+
+    cout << ans << endl;
+
+    return 0;
+}
